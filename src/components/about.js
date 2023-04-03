45d163bef03358e17812426215083d2a0ecec31a
gabi-rogers23 <gabirogers23@gmail.com>
2023-04-03 15:12:13 -0500

    added img to about page

--- src/components/about.js
+++ src/components/about.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {NavBar} from "./exports"
+const self = require("./assets/Rogers_Gabrielle.jpeg")
 
 const About = () => {
     return(  <> <NavBar/>
     <div className="aboutContainer">
+        <div><img className="self" src={self}></img><h1>HELLO.</h1></div>
         <div className="aboutParagraphs">
-            <h1>HELLO.</h1>
+            
 <br/>
 First and foremost, I am an artist. I love to create connections between people and ideas in surprising and unusual ways. Before Fullstack Academy, I spent 10 years building these connections as a freelance design manager for Pharma/Biopharma Fortune 500 companies. I worked with c-suite executives to help them communicate their ideas and data to their teams in a clear and engaging manner through the use of graphics, charts, and photography. During this time, I learned the importance of flexibility and creativity in problem-solving, as well as the ability to manage client expectations.
 <p/>
