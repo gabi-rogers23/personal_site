03d400cae66af14850e87d85e3c6700ab8ffa3d5
gabi-rogers23 <gabirogers23@gmail.com>
2023-04-03 15:26:36 -0500

    added a starting paragraph and changed the position of hello

--- src/components/about.js
+++ src/components/about.js
@@ -5,11 +5,11 @@ const self = require("./assets/Rogers_Gabrielle.jpeg")
 const About = () => {
     return(  <> <NavBar/>
     <div className="aboutContainer">
-        <div><img className="self" src={self}></img><h1>HELLO.</h1></div>
+        <div className="selfContainer"><img className="self" src={self}></img><h1>HELLO.</h1></div>
         <div className="aboutParagraphs">
             
 <br/>
-First and foremost, I am an artist. I love to create connections between people and ideas in surprising and unusual ways. Before Fullstack Academy, I spent 10 years building these connections as a freelance design manager for Pharma/Biopharma Fortune 500 companies. I worked with c-suite executives to help them communicate their ideas and data to their teams in a clear and engaging manner through the use of graphics, charts, and photography. During this time, I learned the importance of flexibility and creativity in problem-solving, as well as the ability to manage client expectations.
+<div className="start">I'm Gabrielle,</div> an artist who loves to create connections between people and ideas in surprising and unusual ways. Before Fullstack Academy, I spent 10 years building these connections as a freelance design manager for Pharma/Biopharma Fortune 500 companies. I worked with c-suite executives to help them communicate their ideas and data to their teams in a clear and engaging manner through the use of graphics, charts, and photography. During this time, I learned the importance of flexibility and creativity in problem-solving, as well as the ability to manage client expectations.
 <p/>
 <br/>
 Most recently, I worked as a digital court reporter, where I used my communication skills to quickly assess the requirements of each individual job. Despite the creative challenges, I found that there were often little to no analytical challenges in this role. I knew that eventually, I would need to make a career change in order to fully exercise my passion for creativity and problem-solving.
