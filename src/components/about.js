04a914004a9aee33c3c4e86d1e1b996b4d679de1
gabi-rogers23 <gabirogers23@gmail.com>
2023-04-06 11:54:31 -0500

    WIP about me section placement

--- src/components/about.js
+++ src/components/about.js
@@ -1,22 +1,24 @@
 import React from "react";
 const self = require("./assets/Rogers_Gabrielle.jpeg");
-import { FaCrow, FaHotdog } from "react-icons/fa";
+import { FaCrow } from "react-icons/fa";
 import { ImCamera } from "react-icons/im";
 import { MdOutlineDirectionsBike } from "react-icons/md";
+import {GrPersonalComputer} from "react-icons/gr"
 
 const About = () => {
   return (
     <>
-      <h1 className="hello">HELLO!</h1>
+      <h1 className="hello">HELLO!          <div className="start">I'm Gabrielle.</div></h1>
+
       <div className="aboutContainer">
         <div className="selfContainer">
           <img className="self" src={self}></img>
         </div>
         <div className="aboutParagraphs">
-          <div className="start">I'm Gabrielle.</div>
+            <h2><MdOutlineDirectionsBike/> <GrPersonalComputer/> About Me <ImCamera/> <FaCrow/></h2>
           <div>
             <p>
-              A passionate developer & designer excited about creating
+              I am a passionate developer & designer excited about creating
               meaningful connections between people and their ideas. I have a
               Bachelor of Fine Arts in Photography and a certificate in
               fullstack web development from Fullstack Academy.
@@ -25,10 +27,7 @@ const About = () => {
             <p>
               When I'm not coding I can be found riding my bike through the
               streets of Chicago , making art, or at the Nature Museum preparing
-              bird specimens for collections.              <MdOutlineDirectionsBike className="navIcon" />
-              <FaHotdog className="navIcon" />
-              <ImCamera className="navIcon" />
-              <FaCrow className="navIcon" />
+              bird specimens for collections.              
             </p>
           </div>
         </div>
