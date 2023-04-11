231ffa8116a2eab96157fd2d39ef1c9a76937c2f
gabi-rogers23 <gabirogers23@gmail.com>
2023-04-11 15:18:21 -0500

    added all project data

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,29 +1,104 @@
 import React from "react";
-import {Navigate} from "react-router-dom"
 const stranger = require("./assets/Stranger'sThings.png");
+const books = require("./assets/InTheBooks.png")
+const fitness = require("./assets/fitness.png")
+const mancala = require("./assets/Mancala.png")
 import { IoIosOpen } from "react-icons/io";
 import { TbBrandGithub } from "react-icons/tb";
 
 const Projects = () => {
-    const navigate = Navigate();
   return (
     <>
-      <h2 className="hello">PAST PROJECTS</h2>
+      <h2 className="hello">PROJECTS</h2>
       <div className="projectsContainer">
         <div className="projectSection">
           <div className="project">
             <img className="projectImg" src={stranger}></img>
-            <div className="overlay"><h2 className="sectionTitle"> STRANGER'S THINGS</h2><div>An application where users can sign-in to buy and sell items.</div>
-            <div>| React.js | CSS | HTML |</div>
-            <div><button onClick={(e)=>{
-                e.preventDefault()
-                location.href="https://github.com/gabi-rogers23"
-            }}><IoIosOpen/></button><button><TbBrandGithub/></button></div>
+            <div className="overlay">
+              <h2 className="sectionTitle"> STRANGER'S THINGS</h2>
+              <div>
+                An application where users can sign-in to buy and sell items.
+              </div>
+              <div>React | CSS | HTML </div>
+              <div>
+              <a href="https://nimble-gaufre-7509d1.netlify.app" target="_blank">
+                <button>View Site
+                  <IoIosOpen />
+                </button></a>
+                <a href="https://github.com/gabi-rogers23/StrangersThings" target="_blank">
+                  <button>
+                    GitHub
+                    <TbBrandGithub />
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
           <div className="project">
-            <h2 className="sectionTitle">In The Books</h2>
+            <img className="projectImg" src={books}></img>
+            <div className="overlay">
+              <h2 className="sectionTitle"> IN THE BOOKS</h2>
+              <div>
+              An e-commerce web application book store.
+              </div>
+              <div>React | CSS | HTML | Express | PostgreSQL | Bcrypt | Node | Faker | Notistack </div>
+              <div>
+              <a href="https://brilliant-daffodil-f42560.netlify.app" target="_blank">
+                <button>View Site
+                  <IoIosOpen />
+                </button></a>
+                <a href="https://github.com/InTheBooks/in-the-books-shop" target="_blank">
+                  <button>
+                    GitHub
+                    <TbBrandGithub />
+                  </button>
+                </a>
+              </div>
+            </div>
           </div>
+
+          <div className="project">
+            <img className="projectImg" src={fitness}></img>
+            <div className="overlay">
+              <h2 className="sectionTitle"> FITNESS TRACK.R </h2>
+              <div>
+              An application where users can sign-in to create and track exercise routines.
+              </div>
+              <div>Express | PostgreSQL | Bcrypt | Node  </div>
+              <div>
+                <a href="https://github.com/gabi-rogers23/Fitness_Track.r" target="_blank">
+                  <button>
+                    GitHub
+                    <TbBrandGithub />
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="project">
+            <img className="projectImg" src={mancala}></img>
+            <div className="overlay">
+              <h2 className="sectionTitle"> MANCALA</h2>
+              <div>
+                Play the game Mancala!
+              </div>
+              <div>Javascript | HTML | CSS</div>
+              <div>
+              <a href="https://effortless-pastelito-8a73c3.netlify.app" target="_blank">
+                <button>View Site
+                  <IoIosOpen />
+                </button></a>
+                <a href="https://github.com/gabi-rogers23/arcade.Mancala" target="_blank">
+                  <button>
+                    GitHub
+                    <TbBrandGithub />
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </>
