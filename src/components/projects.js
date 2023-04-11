13611b2e8e619e4311d0c9a462b07c1f63a70bbc
gabi-rogers23 <gabirogers23@gmail.com>
2023-04-11 10:16:26 -0500

    WIP link to outside webpage from onClick

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,7 +1,11 @@
 import React from "react";
+import {Navigate} from "react-router-dom"
 const stranger = require("./assets/Stranger'sThings.png");
+import { IoIosOpen } from "react-icons/io";
+import { TbBrandGithub } from "react-icons/tb";
 
 const Projects = () => {
+    const navigate = Navigate();
   return (
     <>
       <h2 className="hello">PAST PROJECTS</h2>
@@ -9,7 +13,12 @@ const Projects = () => {
         <div className="projectSection">
           <div className="project">
             <img className="projectImg" src={stranger}></img>
-            <div className="overlay"><h2 className="sectionTitle"> STRANGER'S THINGS</h2><div>Tech Stack: </div>
+            <div className="overlay"><h2 className="sectionTitle"> STRANGER'S THINGS</h2><div>An application where users can sign-in to buy and sell items.</div>
+            <div>| React.js | CSS | HTML |</div>
+            <div><button onClick={(e)=>{
+                e.preventDefault()
+                location.href="https://github.com/gabi-rogers23"
+            }}><IoIosOpen/></button><button><TbBrandGithub/></button></div>
             </div>
           </div>
           <div className="project">
