0742c86b8c128223921db0d5d9aae31bc12cfa18
gabi-rogers23 <gabirogers23@gmail.com>
2023-04-11 10:05:12 -0500

    WIP projects page -- started CSS

--- src/components/about.js
+++ src/components/about.js
@@ -3,20 +3,25 @@ const self = require("./assets/Rogers_Gabrielle.jpeg");
 import { FaCrow, FaHotdog } from "react-icons/fa";
 import { ImCamera } from "react-icons/im";
 import { MdOutlineDirectionsBike, MdComputer } from "react-icons/md";
-import {IoMdFlower} from "react-icons/io";
+import { IoMdFlower } from "react-icons/io";
 
 const About = () => {
   return (
     <>
-      <h1 className="hello">HELLO!          <div className="start">I'm Gabrielle.</div></h1>
+      <h1 className="hello">
+        HELLO! <div className="start">I'm Gabrielle.</div>
+      </h1>
 
       <div className="aboutContainer">
         <div className="selfContainer">
           <img className="self" src={self}></img>
         </div>
-        <div className="iconLine"><MdComputer/>  <MdOutlineDirectionsBike/> <FaHotdog/> <ImCamera/> <IoMdFlower/> <FaCrow/></div>
+        <div className="iconLine">
+          <MdComputer /> <MdOutlineDirectionsBike /> <FaHotdog /> <ImCamera />{" "}
+          <IoMdFlower /> <FaCrow />
+        </div>
         <div className="aboutParagraphs">
-            <h2>About Me </h2>
+          <h2 className="sectionTitle">About Me </h2>
           <div>
             <p>
               I am a passionate developer & designer excited about creating
@@ -26,7 +31,9 @@ const About = () => {
             </p>
 
             <p>
-              When I'm not coding I can be found riding my bike through the streets of Chicago, making art, or at the Nature Museum preparing bird specimens for collections.              
+              When I'm not coding I can be found riding my bike through the
+              streets of Chicago, making art, or at the Nature Museum preparing
+              bird specimens for collections.
             </p>
           </div>
         </div>
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,9 +1,24 @@
 import React from "react";
+const stranger = require("./assets/Stranger'sThings.png");
 
 const Projects = () => {
-    return(<div className="projectsContainer">
-        This is the Projects Page
-    </div>)
-}
+  return (
+    <>
+      <h2 className="hello">PAST PROJECTS</h2>
+      <div className="projectsContainer">
+        <div className="projectSection">
+          <div className="project">
+            <img className="projectImg" src={stranger}></img>
+            <div className="overlay"><h2 className="sectionTitle"> STRANGER'S THINGS</h2><div>Tech Stack: </div>
+            </div>
+          </div>
+          <div className="project">
+            <h2 className="sectionTitle">In The Books</h2>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Projects;
