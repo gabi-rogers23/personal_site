8e4c75dfab21f6453b9271cf1d5347de1fcb69cc
gabi-rogers23 <gabirogers23@gmail.com>
2023-04-12 09:45:34 -0500

    skillStack section complete

--- src/components/skillStack.js
+++ src/components/skillStack.js
@@ -1,27 +1,22 @@
 import React from "react";
-import { SiJavascript, SiReactrouter } from "react-icons/si";
+import { SiJavascript, SiReactrouter, SiPostgresql, SiGithub, SiAdobecreativecloud } from "react-icons/si";
 import { GrReactjs } from "react-icons/gr";
 import {TiHtml5, TiCss3} from "react-icons/ti";
 
 const SkillStack = () => {
   return (
     <>
-      <div className="mainTitle">SKILLS</div>
+      <div className="mainTitle">TOOL KIT</div>
       <div className="skillsContainer">
         <ul className="iconSkills">
-          <li>
-            <SiJavascript className="icon"/>Javascript
-          </li>
-          <li>
-          <GrReactjs className="icon"/> React
-          </li>
-          <li>
-            <SiReactrouter className="icon"/>React Router
-          </li>
-          <li>
-          <TiHtml5 className="icon"/> HTML 5
-          </li>
-          <li><TiCss3 className="icon"/> CSS3</li>
+          <li><SiJavascript className="icon"/>Javascript<p>Node.js | Express.js</p></li>
+          <li><GrReactjs className="icon"/> React</li>
+          <li><SiReactrouter className="icon"/>React Router</li>
+          <li><TiHtml5 className="icon"/> HTML 5</li>
+          <li><TiCss3 className="icon"/> CSS 3</li>
+          <li><SiPostgresql className="icon"/>PostgreSQL</li>
+          <li><SiGithub className="icon"/>GitHub</li>
+          <li><SiAdobecreativecloud className="icon"/>Adobe Creative Cloud</li>
         </ul>
       </div>
     </>
