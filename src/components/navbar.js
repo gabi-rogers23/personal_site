4c2caadbf7f5b12e8b821de85cf0aff1811cfdfd
gabi-rogers23 <gabirogers23@gmail.com>
2023-04-17 16:03:40 -0500

    WIP fadeOut navBar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,31 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { CgCloseO } from "react-icons/cg";
 
 function NavBar(props) {
   return (
     <div className="headerContainer">
-      <button className="menuIcon"
-      onClick={((e)=>{
-        e.preventDefault();
-        props.setShowMenu(false)
-      })}><CgCloseO className="cake"/></button>
-        <nav className="navContainer">
-          <NavLink to="/" end className="nav_link">
-          </NavLink>
-          <NavLink to="/about" className="nav_link">
-            About
-          </NavLink>
-          <NavLink to="/projects" className="nav_link">
-            Projects
-          </NavLink>
-          <NavLink>
-            Skills
-          </NavLink>
-          <NavLink>
-            Connect
-          </NavLink>
-        </nav>
+      <button
+        className="menuIcon"
+        onClick={(e) => {
+          e.preventDefault();
+          props.setShowMenu(false);
+        }}
+      >
+        <CgCloseO className="cake" />
+      </button>
+      <nav className="navContainer">
+        <NavLink to="/" end className="nav_link"></NavLink>
+        <NavLink to="/about" className="nav_link">
+          About
+        </NavLink>
+        <NavLink to="/projects" className="nav_link">
+          Projects
+        </NavLink>
+        <NavLink>Skills</NavLink>
+        <NavLink>Connect</NavLink>
+      </nav>
     </div>
   );
 }
