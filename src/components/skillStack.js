b9f872fc5691a1a1acb46f6a25fc9d5ba10b47a6
gabi-rogers23 <contact@gabriellerogers.io>
2023-10-16 12:40:20 -0500

    added new skills - Java, Spring Boot Salesforce

--- src/components/skillStack.js
+++ src/components/skillStack.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { SiJavascript, SiReactrouter, SiPostgresql, SiGithub, SiAdobecreativecloud } from "react-icons/si";
+import { SiJavascript, SiReactrouter, SiPostgresql, SiAdobecreativecloud } from "react-icons/si";
 import { GrReactjs } from "react-icons/gr";
 import {TiHtml5, TiCss3} from "react-icons/ti";
+import { BiLogoSpringBoot, BiLogoJava, BiLogoGit } from "react-icons/bi";
+import { FaSalesforce } from "react-icons/fa";
+import {TbBrandRedux} from "react-icons/tb";
 
 const SkillStack = () => {
   return (
@@ -11,13 +14,17 @@ const SkillStack = () => {
         <ul className="iconSkills">
           <li><SiJavascript className="icon"/>Javascript<p>Node.js | Express.js</p></li>
           <li><GrReactjs className="icon"/> React</li>
-          <li><SiReactrouter className="icon"/>React Router</li>
+          <li><TbBrandRedux className="icon"/>Redux</li>
           <li><TiHtml5 className="icon"/> HTML 5</li>
           <li><TiCss3 className="icon"/> CSS 3</li>
+          <li><BiLogoSpringBoot className="icon"/>Spring Boot</li>
+          <li><BiLogoJava className="icon"/>Java</li>
+          <li><FaSalesforce className="icon"/>Salesforce</li>
           <li><SiPostgresql className="icon"/>PostgreSQL</li>
-          <li><SiGithub className="icon"/>GitHub</li>
+          <li><BiLogoGit className="icon"/>Git</li>
           <li><SiAdobecreativecloud className="icon"/>Adobe Creative Cloud</li>
-        </ul>
+
+          </ul>
       </div>
     </>
   );
